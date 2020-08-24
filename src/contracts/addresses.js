692b816dd27ae4bc77bb20f5891cd6320f272800
Jikun Liao <liaoJikundever@gmail.com>
2020-08-23 21:34:46 -0500

    Init

---
+++ src/contracts/addresses.js
@@ -0,0 +1,10 @@
+const addresses = {
+  presale: '',
+  daofund: '',
+  teamfund: '',
+  redeemer: '',
+  timer: '',
+  token: '0xd379700999f4805ce80aa32db46a94df64561108'
+};
+
+export default addresses;
